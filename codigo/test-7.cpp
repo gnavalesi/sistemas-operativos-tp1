f392e7464d7955e962db84e8b28a6b65cce0d958
Angel Yan <angelyan96@gmail.com>
2018-04-18 23:15:38 -0300

    Muevo funciones para medir tiempo a un archivo aparte

--- codigo/test-7.cpp
+++ codigo/test-7.cpp
@@ -1,26 +1,11 @@
 #include "ConcurrentHashMap.hpp"
+#include "tiempo.hpp"
 
 #include <cassert>
 #include <iostream>
 #include <list>
-#include <time.h>
 
 using namespace std;
-
-timespec gettime() {
-	timespec time;
-	if (clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time) == -1) {
-		cerr << "Error: unable to get time" << endl;
-		exit(1);
-	}
-	return time;
-}
-
-long timediff(const timespec &start, const timespec &end) {
-	long s = end.tv_sec - start.tv_sec;
-	long ns = end.tv_nsec - start.tv_nsec;
-	return s * 1e9 + ns;
-}
 
 int main(int argc, char const *argv[]) {
 
